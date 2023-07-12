e6a2f6fab8a4691162750764672418c4d7ef6ad5
Влад Шарапа <vl.sharapa20@gmail.com>
2023-07-12 21:51:41 +0300

    Setting url for add page

--- web/app_project/app/urls.py
+++ web/app_project/app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("date/", views.get_week, name='get_week'),
+    path("add/", views.add_page, name= 'add_page'),
+    path('add/add_data/', views.add_data),
 ]
