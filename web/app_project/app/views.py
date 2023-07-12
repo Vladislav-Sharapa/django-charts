abd62c361efbc5d66c9706368ca9700258a00bf7
Влад Шарапа <vl.sharapa20@gmail.com>
2023-07-12 21:38:54 +0300

    Create view and template for add page

--- web/app_project/app/views.py
+++ web/app_project/app/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render, redirect
 def index(request):
     '''Render main page'''
     return render(request, "app/index.html")
+
+def add_data(request):
+    if request.method == "POST":
+        form = MessageLogForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+    form = MessageLogForm()
+
+    return render(request, 'app/add_page.html', {'form': form})
+
+
+
+def add_page(request):
+    form = MessageLogForm(use_required_attribute=False)
+    return render(request, 'app/add_page.html', {'form': form})
+
+
