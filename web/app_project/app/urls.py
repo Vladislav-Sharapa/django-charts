d603f7616fd32b520ca6d37fdba663e9e2fc1337
Влад Шарапа <vl.sharapa20@gmail.com>
2023-07-12 21:53:14 +0300

    Add charts

--- web/app_project/app/urls.py
+++ web/app_project/app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("date/", views.get_week, name='get_week'),
     path("add/", views.add_page, name= 'add_page'),
     path('add/add_data/', views.add_data),
+    path("chart/bar/<str:week>/", views.get_bar_data, name='bar_chart'),
+    path("chart/line/<str:week>/", views.get_line_data, name='line_chart'),
+    path("chart/pie/<str:week>/", views.get_pie_data, name='pie_chart'),
+    path("chart/area/<str:week>/", views.get_area_data, name='area_chart')
 ]
--- web/app_project/app/views.py
+++ web/app_project/app/views.py
@@ -22,3 +22,95 @@ def add_page(request):
     return render(request, 'app/add_page.html', {'form': form})
 
 
+def get_bar_data(request, week) -> JsonResponse:
+    '''Get data for bar chart'''
+
+    if Messagelog.objects.filter(date_rcv__week=week).exists():
+        instances = Messagelog.objects.filter(date_rcv__week=week)
+        instances_count = instances.values('type_doc').annotate(count=Count('type_doc'))
+
+        return JsonResponse({
+            "title": "Bar chart",
+            "data": {
+            "labels": [x['type_doc'] for x in instances_count],
+                "datasets": [{
+                    "label": 'DOCUMENTS PROCESSED',
+                    "data": [x['count'] for x in instances_count],
+                    "borderWidth": 1,
+                    "backgroundColor": ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
+
+            }]}
+        })
+    return HttpResponse("No data for bar chart")
+    
+
+def get_line_data(request, week) -> JsonResponse:
+    '''Get data for line chart'''
+
+    if Messagelog.objects.filter(date_rcv__week=week).exists():
+        instances = Messagelog.objects.filter(date_rcv__week=week)
+        instances_count = instances.values('date_rcv').annotate(count=Count('type_doc'))
+
+        return JsonResponse({
+            "title": 'Line chart',
+            "data": {
+            "labels": [x['date_rcv'] for x in instances_count],
+            "datasets": [{
+                "label": "DOCUMENTS PROCESSED",
+                "data": [x['count'] for x in instances_count],
+                "backgroundColor": ['yellow', 'aqua', 'pink', 'lightgreen'],
+                "borderColor": ['black'],
+                "borderWidth": 0.5,
+                "pointRadius": 5,
+            }],
+        },
+        })
+    
+    return HttpResponse("No data for line chart")
+
+
+def get_pie_data(request, week) -> JsonResponse:
+    '''Get data for pie chart'''
+
+    if Messagelog.objects.filter(date_rcv__week=week).exists():
+        instances = Messagelog.objects.filter(date_rcv__week=week)
+        instances_count = instances.annotate(concatenated=Concat('custom', Value('-'),'pto')).values('concatenated').annotate(count=Count('*'))
+    
+        return JsonResponse({
+            "title": 'pie chart',
+            'data': {
+            'labels': [x['concatenated'] for x in instances_count],
+            'datasets': [{
+                'label': "PROCESSED",
+                'data': [x['count'] for x in instances_count],
+                'backgroundColor': ['yellow', 'aqua', 'pink', 'lightgreen', 'gold', 'lightblue'],
+                'borderWidth': 1,
+                'borderRadius': 2,
+                'hoverOffset': 5,
+            }],
+            }
+        })
+    
+    return HttpResponse("No data for pie chart")
+
+
+def get_area_data(request, week) -> JsonResponse:
+    '''Get data for area chart'''
+
+    if Messagelog.objects.filter(date_rcv__week=week).exists():
+        instances = Messagelog.objects.filter(date_rcv__week=week)
+        instances_count = instances.values('declarant_unp').annotate(count=Count('*'))
+
+        return JsonResponse({
+            'title': 'Area chart',
+            'data': {
+            'labels': [x['declarant_unp'] for x in instances_count],
+            'datasets': [{
+            'label': "PROCESSED",
+            'data': [x['count'] for x in instances_count],
+            'backgroundColor': ['yellow', 'aqua', 'pink', 'lightgreen', 'gold', 'lightblue'],
+            }],
+        }
+        })
+    
+    return HttpResponse("No data for area chart")
